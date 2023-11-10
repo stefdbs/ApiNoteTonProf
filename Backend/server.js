fab9c4b1df1fd7f9ef9b8d12eacc013142d8431a
stefdbs <you@example.com>
2023-11-10 17:23:43 +0100

    fusion de app.js et server.js ok, mais ça ne regle pas les references circulaires

--- Backend/server.js
+++ Backend/server.js
@@ -1,5 +1,5 @@
 /* Import des modules necessaires */
-const app = require("./app");
+const express = require("express");
 const dotenv = require("dotenv").config({ encoding: "latin1" });
 const mongoose = require("mongoose");
 
@@ -7,7 +7,73 @@ const mongoose = require("mongoose");
 let DB = require('./db.config')
 let MONGODB_URL = require('./db.config')
 
-/* Connection BDD mongoose */
+/*****************************/
+/*** Initialisation de l'API */
+const app = express();
+
+app.use(express.urlencoded({ extended: true }))
+
+app.use(express.json());
+
+/* Mise en place reponses headers */
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, x-access-token, role, Content, Accept, Content-Type, Authorization"
+    );
+    res.setHeader(
+        "Access-Control-Allow-Methods",
+        "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+    );
+    next();
+});
+
+/* Securite en tete */
+// const helmet = require("helmet");
+
+// app.use(helmet({
+//     crossOriginResourcePolicy: false,
+// }));
+
+// /* RateLimit */
+// const rateLimit = require("express-rate-limit");
+
+// app.use(
+//     rateLimit({
+//         windowMs: 10 * 60 * 1000,
+//         max: 100,
+//         message:
+//             "Vous avez effectué plus de 100 requêtes dans une limite de 10 minutes!",
+//         headers: true,
+//     })
+// );
+
+
+// ROUTERS 
+const FormationRoutes = require("./routes/formation.routes");
+
+//routage principal
+app.get('/', (req, res, next) => res.send('im online good job'))
+
+//app.use("/formations", FormationRoutes)
+
+
+app.all('*', (req, res) => res.status(501).send('you are lost poor baby'))
+/***********************************/
+
+//routage
+
+
+//const UserRoutes = require("./routes/user.routes");
+
+// app.use("/api/auth", UserRoutes);
+//app.use("/api/sauces", SauceRoutes);
+
+
+
+
+/* démarrage de l'API et Connection BDD mongoose */
 /********************************/
 /*** Start serveur avec test DB */
 
